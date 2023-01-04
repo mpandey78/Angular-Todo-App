fd1bf081d7d08f5f71e397dee9d3ad2b81bf60fb
mpandey78 <mp67854@gmail.com>
2023-01-04 12:25:04 +0530

    code updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { ToDoAppComponent } from './to-do-app/to-do-app.component';
+import { FieldErrorDisplayComponent } from './field-error-display/field-error-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
-    ToDoAppComponent
+    ToDoAppComponent,
+    FieldErrorDisplayComponent
   ],
   imports: [
     BrowserModule,
